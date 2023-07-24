bbea5ae64ad36a81800b57ccc10123d2d32fef10
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-24 17:53:23 +0200

    feat random

---
+++ Dadi/JS/main.js
@@ -0,0 +1,21 @@
+let randomUser = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomUser);
+
+
+let randomComputer = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomComputer);
+
+if (randomUser > randomComputer) {
+
+    console.log('L\'utente ha il punteggio più alto');
+
+} else if (randomUser < randomComputer) {
+
+    console.log('Il computer ha il punteggio più alto');
+
+} else (randomUser == randomComputer) {
+
+    console.log('Avete lo stesso punteggio, quindi avete pareggiato');
+}
