3069d7e5126869456141aa19618722b14205a89b
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-24 17:23:35 +0200

    feat valid

--- Mail/JS/main.js
+++ Mail/JS/main.js
@@ -2,28 +2,33 @@ const mailArray = ["Admin@gmail.com", "Employer@gmail.com", "Supervisor@gmail.co
 
 const emailUser = prompt("Inserisci l'email per accedere");
 
+let valid = false;
 
 for (let i = 0; i < mailArray.length; i++) {
 
 
     if (emailUser == mailArray[i]) {
 
+        valid = true;
+
         console.log('corretto ' + emailUser);
+
 
     } else {
 
         console.log('sbagliata ' + emailUser);
     }
-  
-    
+
 }
 
-/*
 
-if (mailFound == emailUser) {
-    
+
+if (valid) {
+
     alert('Accesso consentito');
+
 } else {
+
     alert('Accesso non consentito prova con un\'altra email');
 }
-*/
+
